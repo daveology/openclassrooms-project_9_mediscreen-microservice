78faa216ad532217c4160b30af9cbca59dcd6bc1
David <cheery.spot1750@fastmail.com>
2021-12-29 22:02:02 +0100

    build: update Patient model from patient microservice information

--- src/main/java/com/mediscreen/model/Patient.java
+++ src/main/java/com/mediscreen/model/Patient.java
@@ -1,6 +1,94 @@
 package com.mediscreen.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Patient {
 
-    
+    private Long patientId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Patient() {}
+
+    public Patient(String firstName, String lastName, String gender,
+                   LocalDate birthDate, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return Objects.equals(patientId, patient.patientId) && Objects.equals(firstName, patient.firstName)
+                && Objects.equals(lastName, patient.lastName) && Objects.equals(gender, patient.gender)
+                && Objects.equals(birthDate, patient.birthDate) && Objects.equals(address, patient.address)
+                && Objects.equals(phone, patient.phone);
+    }
 }
