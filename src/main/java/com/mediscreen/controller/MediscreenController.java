fca9197c29fe47daf005d7324785c02efb5654c0
David <cheery.spot1750@fastmail.com>
2021-12-29 22:08:31 +0100

    feat: add homepage controller's endpoint

--- src/main/java/com/mediscreen/controller/MediscreenController.java
+++ src/main/java/com/mediscreen/controller/MediscreenController.java
@@ -1,9 +1,15 @@
 package com.mediscreen.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MediscreenController {
 
+    @RequestMapping("/")
+    public String home(Model model) {
 
+        return "home";
+    }
 }
