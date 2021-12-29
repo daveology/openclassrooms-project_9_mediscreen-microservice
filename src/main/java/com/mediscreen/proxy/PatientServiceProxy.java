088a53b2cc69c20ed2b7526a4435f45983d22511
David <cheery.spot1750@fastmail.com>
2021-12-29 23:32:06 +0100

    feat: add PatientList proxy endpoint

--- src/main/java/com/mediscreen/proxy/PatientServiceProxy.java
+++ src/main/java/com/mediscreen/proxy/PatientServiceProxy.java
@@ -1,9 +1,14 @@
 package com.mediscreen.proxy;
 
+import com.mediscreen.model.Patient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @FeignClient(name = "Patient", url = "localhost:7911")
 public interface PatientServiceProxy {
 
-
+    @GetMapping(value="/patientList")
+    Collection<Patient> getPatientList();
 }
