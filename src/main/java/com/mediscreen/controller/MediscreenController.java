e80da8f203f8991617648ba637d8ba1afd133edb
David <cheery.spot1750@fastmail.com>
2021-12-30 00:55:18 +0100

    build: add javax validation

--- src/main/java/com/mediscreen/controller/MediscreenController.java
+++ src/main/java/com/mediscreen/controller/MediscreenController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class MediscreenController {
