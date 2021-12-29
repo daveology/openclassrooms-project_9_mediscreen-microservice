4800c3048e9372c5b422cee7e371aacae3982b69
David <cheery.spot1750@fastmail.com>
2021-12-29 17:18:26 +0100

    refacto: reanme Openclassrooms9thProjectMediscreenMicroserviceApplication to MediscreenApplication

--- src/main/java/com/mediscreen/MediscreenApplication.java
+++ src/main/java/com/mediscreen/MediscreenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Openclassrooms9thProjectMediscreenMicroserviceApplication {
+public class MediscreenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Openclassrooms9thProjectMediscreenMicroserviceApplication.class, args);
+        SpringApplication.run(MediscreenApplication.class, args);
     }
 
 }
--- src/test/java/com/mediscreen/MediscreenApplicationTests.java
+++ src/test/java/com/mediscreen/MediscreenApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Openclassrooms9thProjectMediscreenMicroserviceApplicationTests {
+class MediscreenApplicationTests {
 
     @Test
     void contextLoads() {
