0a1a1e835c5ecddc187efa925bc3dbd35b1c164c
David <cheery.spot1750@fastmail.com>
2021-12-29 23:13:01 +0100

    feat: add error 404 endpoint

--- src/main/java/com/mediscreen/controller/MediscreenController.java
+++ src/main/java/com/mediscreen/controller/MediscreenController.java
@@ -2,7 +2,12 @@ package com.mediscreen.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MediscreenController {
@@ -11,5 +16,20 @@ public class MediscreenController {
     public String home(Model model) {
 
         return "home";
+    }
+
+    @GetMapping("error")
+    public ModelAndView error(HttpServletRequest request, Model model) {
+        ModelAndView mav = new ModelAndView();
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == 404) {
+                String errorMessage = "La page est introuvable.";
+                mav.addObject("errorMsg", errorMessage);
+                mav.setViewName("404");
+            }
+        }
+        return mav;
     }
 }
