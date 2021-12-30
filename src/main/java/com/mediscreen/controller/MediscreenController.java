3bde6b745f7a36027ef80e2d8f3a6fb5b0e927ce
David <cheery.spot1750@fastmail.com>
2021-12-30 14:51:39 +0100

    fix: remove error endpoint executed automatically by Thymeleaf

--- src/main/java/com/mediscreen/controller/MediscreenController.java
+++ src/main/java/com/mediscreen/controller/MediscreenController.java
@@ -72,18 +72,4 @@ public class MediscreenController {
         model.addAttribute("patientList", mediscreenService.readPatientList());
         return "redirect:/patientList";
     }
-
-    @GetMapping("error")
-    public ModelAndView error(HttpServletRequest request) {
-
-        ModelAndView mav = new ModelAndView();
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-            if (statusCode == 404) {
-                mav.setViewName("404");
-            }
-        }
-        return mav;
-    }
 }
