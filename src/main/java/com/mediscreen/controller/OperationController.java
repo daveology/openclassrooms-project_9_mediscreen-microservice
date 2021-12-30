3c0b2701b95d3bcfba89c2db414c6c24df333043
David <cheery.spot1750@fastmail.com>
2021-12-30 18:16:09 +0100

    feat: add addPatient into OperationController

--- src/main/java/com/mediscreen/controller/OperationController.java
+++ src/main/java/com/mediscreen/controller/OperationController.java
@@ -1,9 +1,19 @@
 package com.mediscreen.controller;
 
+import com.mediscreen.model.Patient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class OperationController {
 
+    @PostMapping("/add")
+    public Patient addPatient(@RequestParam String family, @RequestParam String given, @RequestParam LocalDate dob,
+                              @RequestParam String address, @RequestParam String phone) {
 
+        return patientServiceProxy.addPatient(family, given, dob, address, phone);
+    }
 }
