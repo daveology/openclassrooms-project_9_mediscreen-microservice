877594a65bc760965cddeaab30557b709ed303e8
David <cheery.spot1750@fastmail.com>
2022-01-01 18:20:16 +0100

    feat: add generateReport proxy endpoint

--- src/main/java/com/mediscreen/proxy/ReportServiceProxy.java
+++ src/main/java/com/mediscreen/proxy/ReportServiceProxy.java
@@ -1,9 +1,17 @@
 package com.mediscreen.proxy;
 
+import com.mediscreen.model.Note;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
 
 @FeignClient(name = "Report", url = "localhost:7933")
 public interface ReportServiceProxy {
 
-
+    @GetMapping(value="/generateReport")
+    Long generateReport(@RequestParam("age") int age,
+                                  @RequestParam("gender") int gender,
+                                  Collection<Note> noteList);
 }
