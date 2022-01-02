3beeecd1885955255e495d00c69e3d3df3c45846
David <cheery.spot1750@fastmail.com>
2022-01-02 19:52:33 +0100

    feat: add controllers endpoint addNote

--- src/main/java/com/mediscreen/controller/OperationController.java
+++ src/main/java/com/mediscreen/controller/OperationController.java
@@ -1,6 +1,8 @@
 package com.mediscreen.controller;
 
+import com.mediscreen.model.Note;
 import com.mediscreen.model.Patient;
+import com.mediscreen.proxy.NoteServiceProxy;
 import com.mediscreen.proxy.PatientServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ public class OperationController {
 
     @Autowired
     private PatientServiceProxy patientServiceProxy;
+    @Autowired
+    private NoteServiceProxy noteServiceProxy;
 
     @PostMapping("/patient/add")
     public Patient addPatient(@RequestParam String family, @RequestParam String given, @RequestParam String dob,
@@ -21,5 +25,16 @@ public class OperationController {
 
         Patient patient = new Patient(family, given, sex, LocalDate.parse(dob), address, phone);
         return patientServiceProxy.addPatient(patient);
+    }
+
+    @PostMapping("/patHistory/add")
+    public Note addNote(@RequestParam Long patId, @RequestParam String e) {
+
+        Note note = new Note();
+        note.setPatientId(patId);
+        note.setNoteDate(LocalDate.now());
+        note.setContent(e);
+
+        return noteServiceProxy.createNote(note);
     }
 }
